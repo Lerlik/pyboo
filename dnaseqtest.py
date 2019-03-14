de54c1ac730388ee54203592aff47496d541dd19
Lerlik <csfLelik@gmail.com>
2019-03-13 21:45:57 -0700

    sdf

--- dnaseqtest.py
+++ dnaseqtest.py
@@ -12,7 +12,7 @@ sequence = {"A", "G", "C", "T"}
 samples = []
 samples.append(CDnaSample("AKJSCYHFDGCYAASDWEOTAGCKSDJHJ", sequence))
 samples.append(CDnaSample("WHUCUHWAGCTSIDCLIIWEISKDJCVMNSD", sequence))
-sensors = {CDnaSensor("AGCT"), CDnaSensor("TACG"), CDnaSensor("GUCA"), CDnaSensor("AGTC")}
+sensors = { CDnaSensor("AGCT"), CDnaSensor("TACG"), CDnaSensor("GUCA"), CDnaSensor("AGTC") }
 for sampl in samples:
     device = CDnaSequencer("Robert Bruce Banner", sampl, sensors)
     device.analyze()
